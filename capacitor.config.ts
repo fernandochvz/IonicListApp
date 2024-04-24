79ce313c95379d8d52eda70a83cefc17a2dd3723
fernandochvz <ferku_c10@hotmail.com>
2024-04-24 16:22:16 +0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-react-capacitor-course',
+  webDir: 'dist'
+};
+
+export default config;
